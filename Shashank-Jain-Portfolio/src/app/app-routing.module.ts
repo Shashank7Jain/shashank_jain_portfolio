2db0728bce7473a6616e04dc3365855180af5d19
sjai6788 <sjai6788@uni.sydney.edu.au>
2020-05-17 21:07:21 +1000

    Revert "minot update on background image change"
    This reverts commit 4eff1562e1bda6b3b0fc2db25d035934b4065489.

--- Shashank-Jain-Portfolio/src/app/app-routing.module.ts
+++ Shashank-Jain-Portfolio/src/app/app-routing.module.ts
@@ -2,13 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ResumeComponent } from "./components/resume/resume.component";
 
-const routes: Routes = [
-  {
-    path: "resume",
-    component: ResumeComponent,
-    data: { imageURL: "../assets/images/resume.jpg" },
-  },
-];
+const routes: Routes = [{ path: "resume", component: ResumeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
