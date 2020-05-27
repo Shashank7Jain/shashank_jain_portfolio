6a0e07fe878e2f2bcbdbe388afeb76da272e46fe
sjai6788 <sjai6788@uni.sydney.edu.au>
2020-05-27 23:57:28 +1000

    footer

--- Shashank-Jain-Portfolio/src/app/app.module.ts
+++ Shashank-Jain-Portfolio/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ResumeComponent } from "./components/resume/resume.component";
+import { FooterComponent } from './commons/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, ResumeComponent],
+  declarations: [AppComponent, ResumeComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
